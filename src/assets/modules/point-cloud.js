7f5a75ab4cc421611c3050142e6aa68f00813b5d
Luigui Delyer <github@s1x.com.br>
2017-08-15 17:58:39 -0300

    alteração no point cloud layer, agora ele recebe parametros pra setar o renderer!

--- src/assets/modules/point-cloud.js
+++ src/assets/modules/point-cloud.js
@@ -1,6 +1,6 @@
 import * as arceasy from 'arceasy'
 
-const run = () => {
+const run = renderer => {
     const map = arceasy.obj.map
     const PointCloudLayer = arceasy.obj.constructors.layer.PointCloudLayer
     const PointCloudUniqueValueRenderer = arceasy.obj.constructors.renderer.PointCloudUniqueValueRenderer
@@ -38,142 +38,254 @@ const run = () => {
     ******************************************************************/
 
     // Display point cloud based on original scan colors/using the RGB values
+    //
+    // var pointCloudRGBRenderer = new PointCloudRGBRenderer({
+    //     field: 'RGB'
+    // })
+    //
+    // // Display point cloud based on unique values in an attribute
+    // // Instead of using a constructor you can also create the renderer from a JSON file
+    //
+    // var pointCloudUniqueValueRenderer = new PointCloudUniqueValueRenderer({
+    //     field: 'CLASS_CODE', // field containing data for standard LAS classification
+    //     colorUniqueValueInfos: [
+    //         {
+    //             values: ['1'],
+    //             label: 'Unassigned',
+    //             color: [180, 180, 180]
+    //         },
+    //         {
+    //             values: ['2'],
+    //             label: 'Ground',
+    //             color: [222, 184, 135]
+    //         },
+    //         {
+    //             values: ['3'],
+    //             label: 'Low vegetation',
+    //             color: [200, 232, 171]
+    //         },
+    //         {
+    //             values: ['4'],
+    //             label: 'Medium vegetation',
+    //             color: [76, 112, 43]
+    //         },
+    //         {
+    //             values: ['5'],
+    //             label: 'High vegetation',
+    //             color: [76, 112, 43]
+    //         },
+    //         {
+    //             values: ['6'],
+    //             label: 'Building',
+    //             color: [158, 40, 17]
+    //         },
+    //         {
+    //             values: ['7'],
+    //             label: 'Low Point',
+    //             color: [255, 255, 255]
+    //         },
+    //         {
+    //             values: ['8'],
+    //             label: 'Reserved',
+    //             color: [139, 69, 19]
+    //         },
+    //         {
+    //             values: ['9'],
+    //             label: 'Water',
+    //             color: [13, 68, 80]
+    //         },
+    //         {
+    //             values: ['10'],
+    //             label: 'Rail',
+    //             color: [255, 233, 68]
+    //         },
+    //         {
+    //             values: ['11'],
+    //             label: 'Road surface',
+    //             color: [255, 233, 68]
+    //         },
+    //         {
+    //             values: ['12'],
+    //             label: 'Reserved',
+    //             color: [255, 255, 255]
+    //         }]
+    // })
+    //
+    // // Use the PointCloudStretch renderer to map the values of an attribute
+    // // to a continuous color scale
+    //
+    // var pointCloudStretchRenderer = new PointCloudStretchRenderer({
+    //     field: 'ELEVATION', // field containing elevation values
+    //     stops: [{
+    //         value: -0.78,
+    //         color: [61, 51, 158]
+    //     }, {
+    //         value: 1,
+    //         color: [73, 196, 196]
+    //     }, {
+    //         value: 2,
+    //         color: [104, 196, 73]
+    //     }, {
+    //         value: 3,
+    //         color: [235, 232, 84]
+    //     }, {
+    //         value: 4,
+    //         color: [235, 162, 84]
+    //     }, {
+    //         value: 12,
+    //         color: [235, 84, 84]
+    //     }, {
+    //         value: 20,
+    //         color: [100, 100, 100]
+    //     }]
+    // })
+    //
+    // // Classify numerical attribute values and then apply colors based on
+    // // resulting classes using the PointCloudClassBreaksRenderer
+    //
+    // var pointCloudClassBreaksRenderer = new PointCloudClassBreaksRenderer({
+    //     field: 'INTENSITY', // field containing intensity values
+    //     colorClassBreakInfos: [
+    //         {
+    //             minValue: 0,
+    //             maxValue: 30000,
+    //             color: [255, 255, 255]
+    //         }, {
+    //             minValue: 30000,
+    //             maxValue: 50000,
+    //             color: [100, 100, 100]
+    //         }, {
+    //             minValue: 50000,
+    //             maxValue: 65700,
+    //             color: [50, 50, 50]
+    //         }]
+    // })
 
-    var pointCloudRGBRenderer = new PointCloudRGBRenderer({
-        field: 'RGB'
-    })
-
-    // Display point cloud based on unique values in an attribute
-    // Instead of using a constructor you can also create the renderer from a JSON file
-
-    var pointCloudUniqueValueRenderer = new PointCloudUniqueValueRenderer({
-        field: 'CLASS_CODE', // field containing data for standard LAS classification
-        colorUniqueValueInfos: [
-            {
-                values: ['1'],
-                label: 'Unassigned',
-                color: [180, 180, 180]
-            },
-            {
-                values: ['2'],
-                label: 'Ground',
-                color: [222, 184, 135]
-            },
-            {
-                values: ['3'],
-                label: 'Low vegetation',
-                color: [200, 232, 171]
-            },
-            {
-                values: ['4'],
-                label: 'Medium vegetation',
-                color: [76, 112, 43]
-            },
-            {
-                values: ['5'],
-                label: 'High vegetation',
-                color: [76, 112, 43]
-            },
-            {
-                values: ['6'],
-                label: 'Building',
-                color: [158, 40, 17]
-            },
-            {
-                values: ['7'],
-                label: 'Low Point',
-                color: [255, 255, 255]
-            },
-            {
-                values: ['8'],
-                label: 'Reserved',
-                color: [139, 69, 19]
-            },
-            {
-                values: ['9'],
-                label: 'Water',
-                color: [13, 68, 80]
-            },
-            {
-                values: ['10'],
-                label: 'Rail',
-                color: [255, 233, 68]
-            },
-            {
-                values: ['11'],
-                label: 'Road surface',
-                color: [255, 233, 68]
-            },
-            {
-                values: ['12'],
-                label: 'Reserved',
-                color: [255, 255, 255]
-            }]
-    })
-
-    // Use the PointCloudStretch renderer to map the values of an attribute
-    // to a continuous color scale
-
-    var pointCloudStretchRenderer = new PointCloudStretchRenderer({
-        field: 'ELEVATION', // field containing elevation values
-        stops: [{
-            value: -0.78,
-            color: [61, 51, 158]
-        }, {
-            value: 1,
-            color: [73, 196, 196]
-        }, {
-            value: 2,
-            color: [104, 196, 73]
-        }, {
-            value: 3,
-            color: [235, 232, 84]
-        }, {
-            value: 4,
-            color: [235, 162, 84]
-        }, {
-            value: 12,
-            color: [235, 84, 84]
-        }, {
-            value: 20,
-            color: [100, 100, 100]
-        }]
-    })
-
-    // Classify numerical attribute values and then apply colors based on
-    // resulting classes using the PointCloudClassBreaksRenderer
-
-    var pointCloudClassBreaksRenderer = new PointCloudClassBreaksRenderer({
-        field: 'INTENSITY', // field containing intensity values
-        colorClassBreakInfos: [
-            {
-                minValue: 0,
-                maxValue: 30000,
-                color: [255, 255, 255]
+    const renderers = [
+        new PointCloudRGBRenderer({
+            field: 'RGB'
+        }),
+        new PointCloudUniqueValueRenderer({
+            field: 'CLASS_CODE', // field containing data for standard LAS classification
+            colorUniqueValueInfos: [
+                {
+                    values: ['1'],
+                    label: 'Unassigned',
+                    color: [180, 180, 180]
+                },
+                {
+                    values: ['2'],
+                    label: 'Ground',
+                    color: [222, 184, 135]
+                },
+                {
+                    values: ['3'],
+                    label: 'Low vegetation',
+                    color: [200, 232, 171]
+                },
+                {
+                    values: ['4'],
+                    label: 'Medium vegetation',
+                    color: [76, 112, 43]
+                },
+                {
+                    values: ['5'],
+                    label: 'High vegetation',
+                    color: [76, 112, 43]
+                },
+                {
+                    values: ['6'],
+                    label: 'Building',
+                    color: [158, 40, 17]
+                },
+                {
+                    values: ['7'],
+                    label: 'Low Point',
+                    color: [255, 255, 255]
+                },
+                {
+                    values: ['8'],
+                    label: 'Reserved',
+                    color: [139, 69, 19]
+                },
+                {
+                    values: ['9'],
+                    label: 'Water',
+                    color: [13, 68, 80]
+                },
+                {
+                    values: ['10'],
+                    label: 'Rail',
+                    color: [255, 233, 68]
+                },
+                {
+                    values: ['11'],
+                    label: 'Road surface',
+                    color: [255, 233, 68]
+                },
+                {
+                    values: ['12'],
+                    label: 'Reserved',
+                    color: [255, 255, 255]
+                }]
+        }),
+        new PointCloudStretchRenderer({
+            field: 'ELEVATION', // field containing elevation values
+            stops: [{
+                value: -0.78,
+                color: [61, 51, 158]
             }, {
-                minValue: 30000,
-                maxValue: 50000,
+                value: 1,
+                color: [73, 196, 196]
+            }, {
+                value: 2,
+                color: [104, 196, 73]
+            }, {
+                value: 3,
+                color: [235, 232, 84]
+            }, {
+                value: 4,
+                color: [235, 162, 84]
+            }, {
+                value: 12,
+                color: [235, 84, 84]
+            }, {
+                value: 20,
                 color: [100, 100, 100]
-            }, {
-                minValue: 50000,
-                maxValue: 65700,
-                color: [50, 50, 50]
             }]
-    })
+        }),
+        new PointCloudClassBreaksRenderer({
+            field: 'INTENSITY', // field containing intensity values
+            colorClassBreakInfos: [
+                {
+                    minValue: 0,
+                    maxValue: 30000,
+                    color: [255, 255, 255]
+                }, {
+                    minValue: 30000,
+                    maxValue: 50000,
+                    color: [100, 100, 100]
+                }, {
+                    minValue: 50000,
+                    maxValue: 65700,
+                    color: [50, 50, 50]
+                }]
+        })
+    ]
 
     // Assign the renderer to the point cloud layers
 
-    pcLayer.renderer = pointCloudStretchRenderer
+    pcLayer.renderer = renderer !== undefined ? renderers[renderer] : renderers[2]
     map.add(pcLayer)
 
     // Handle change events on radio buttons to switch to the correct renderer
 
-    var rendererType = { // eslint-disable-line
-        rgbRenderer: pointCloudRGBRenderer,
-        uniqueValueRenderer: pointCloudUniqueValueRenderer,
-        stretchRenderer: pointCloudStretchRenderer,
-        classBreaksRenderer: pointCloudClassBreaksRenderer
-    }
+    // var rendererType = { // eslint-disable-line
+    //     rgbRenderer: pointCloudRGBRenderer,
+    //     uniqueValueRenderer: pointCloudUniqueValueRenderer,
+    //     stretchRenderer: pointCloudStretchRenderer,
+    //     classBreaksRenderer: pointCloudClassBreaksRenderer
+    // }
 
     // pcLayer.renderer = rendererType.stretchRenderer
 
@@ -185,9 +297,9 @@ const run = () => {
     // }
 }
 
-const pointCloud = () => {
+const pointCloud = renderer => {
     if (arceasy.obj.view !== '' && arceasy.obj.view !== undefined) {
-        run()
+        run(renderer)
     }
 }
 
